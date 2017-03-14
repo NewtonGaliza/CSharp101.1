6dfe122ffdd90cea31dcb0006a6c946334250caa
NewtonGaliza <newtonjgaliza@gmail.com>
2017-03-13 23:10:07 -0300

    two buttons, one to click, one to stay hover

---
+++ CSharp101.1/CSharp101.1/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CSharp101._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You clicked", "Click");
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("You stay hover", "Stay Hover");
+        }
+    }
+}
